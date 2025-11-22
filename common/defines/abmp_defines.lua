9cf7663803b143d3d6b6ba32ece99b741a4d2f9b
danielm94 <24377240+danielm94@users.noreply.github.com>
2025-11-22 01:49:21 -0500

    Reduce trade power share requirement for target.

---
+++ common/defines/abmp_defines.lua
@@ -0,0 +1,6 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+    NDefines.NDiplomacy.JUSTIFY_TRADE_CONFLICT_LIMIT = 0.1			-- How big share of the trade power needed on the target to be able to justify a trade conflict
+	NDefines.NDiplomacy.JUSTIFY_TRADE_CONFLICT_ACTOR_LIMIT = 0.1	-- How big share of the trade power needed on the actor to be able to justify a trade conflict
